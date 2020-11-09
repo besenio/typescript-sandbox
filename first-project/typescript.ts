68bda322a04e92d0b4c03312870154556f59a875
besenio <kbesenio2@yahoo.com>
2020-11-09 03:48:11 -0800

    types number string boolean

--- first-project/typescript.ts
+++ first-project/typescript.ts
@@ -1,5 +1,3 @@
-export {};
-
 const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
--- notes/02.11_Using_Types.ts
+++
@@ -1,4 +0,0 @@
-// Core Types
-// - Number
-// - String
-// - Boolean (no truthy or falsy values)
