89a211f88b5373cefe8f57c6bafecd4288cda9c0
besenio <kbesenio2@yahoo.com>
2020-11-09 04:58:38 -0800

    types Tuple and Enum

--- sandbox-001/app.js
+++ sandbox-001/app.js
@@ -1,14 +1,45 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
+// const person: {
+//    name: string;
+//    age: number;
+// } = {
+// const person: {
+//    name: string;
+//    age: number;
+//    hobbies: string[];
+//    role: [number, string]
+// } = {
+//    name: 'Kevin',
+//    age: 32,
+//    hobbies: ['Sports', 'Cooking'],
+//    role: [2, 'author']
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'Kevin',
+    age: 32,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) WRONG!!!
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
+}
--- sandbox-001/app.ts
+++ sandbox-001/app.ts
@@ -1,15 +1,47 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-   const result = n1 + n2;
-   if (showResult) {
-      console.log(phrase + result);
-   } else {
-      return result;
-   }
+// const person: {
+//    name: string;
+//    age: number;
+// } = {
+// const person: {
+//    name: string;
+//    age: number;
+//    hobbies: string[];
+//    role: [number, string]
+// } = {
+//    name: 'Kevin',
+//    age: 32,
+//    hobbies: ['Sports', 'Cooking'],
+//    role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
+   name: 'Kevin',
+   age: 32,
+   hobbies: ['Sports', 'Cooking'],
+   role: Role.ADMIN
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+   console.log(hobby.toUpperCase());
+   // console.log(hobby.map()) WRONG!!!
 }
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: '
-
-add(number1, number2, printResult, resultPhrase);
+if (person.role === Role.ADMIN) {
+   console.log('is admin');
+}
