12724dcc11a1602b35ef1e61106deb56660f6372
besenio <kbesenio2@yahoo.com>
2020-11-16 17:43:41 -0800

    function type

--- sandbox-001/app.js
+++ sandbox-001/app.js
@@ -2,6 +2,10 @@ function add(n1, n2) {
     return n1 + n2;
 }
 function printResult(num) {
+    // return type is void because it does not return a value
     console.log('Result: ' + num);
 }
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
--- sandbox-001/app.ts
+++ sandbox-001/app.ts
@@ -8,3 +8,12 @@ function printResult(num: number) {
 }
 
 printResult(add(5, 12));
+
+// function type
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
