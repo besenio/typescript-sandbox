1bc110d663ce677e51ace89eea0eea81f2a33f4b
besenio <kbesenio2@yahoo.com>
2020-11-21 16:26:20 -0800

    class

--- sandbox-003/dist/app.js
+++ sandbox-003/dist/app.js
@@ -1,2 +1,9 @@
 "use strict";
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const accounting = new Department('Accounting');
+console.log(accounting);
 //# sourceMappingURL=app.js.map
