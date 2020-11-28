656cd0516262a8146aed2c062e028c3003c83dbf
besenio <kbesenio2@yahoo.com>
2020-11-27 21:20:31 -0800

    safe guards

--- sandbox-004/src/app.ts
+++ sandbox-004/src/app.ts
@@ -26,3 +26,58 @@ type Numeric = number | boolean;
 
 // intersection types
 type Universal = Combinable & Numeric;
+
+// type guard using typeof
+function add(a: Combinable, b: Combinable) {
+   if (typeof a === 'string' || typeof b === 'string') {
+      return a.toString() + b.toString();
+   }
+   return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+// type guard if in check
+function printEmployeeInformation(emp: UnknownEmployee) {
+   console.log('Name: ' + emp.name);
+   if ('privileges' in emp) {
+      console.log('Privileges: ' + emp.privileges);
+   }
+   if ('startDate' in emp) {
+      console.log('Start Date: ' + emp.startDate);
+   }
+}
+
+printEmployeeInformation(e1);
+
+// instanceof type guard
+class Car {
+   drive() {
+      console.log('Driving a car...');
+   }
+}
+
+class Truck {
+   drive() {
+      console.log('Driving a truck...');
+   }
+
+   loadCargo(amount: number) {
+      console.log('Loading cargo...' + amount);
+   }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+   vehicle.drive();
+   if (vehicle instanceof Truck) {
+      vehicle.loadCargo(1000);
+   }
+}
+
+useVehicle(v1);
+useVehicle(v2);
