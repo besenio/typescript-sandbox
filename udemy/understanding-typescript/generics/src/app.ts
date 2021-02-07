ef98d9e012f896c69360cdf4411dd3fcc8820f9e
besenio <kbesenio2@yahoo.com>
2021-02-07 02:25:06 -0800

    built in generics

---
+++ udemy/understanding-typescript/generics/src/app.ts
@@ -0,0 +1,14 @@
+// // generic type - built in
+// const names: Array<string> = []; // string[]
+// // names[0].split(' ');
+
+// // promise type - built in
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//    setTimeout(() => {
+//       resolve(10);
+//    }, 2000)
+// });
+
+// promise.then(data => {
+//    // data.split(' ');
+// })
