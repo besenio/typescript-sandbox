7dcb3981668b67e46a30fa4b4daccdb1079c61da
besenio <kbesenio2@yahoo.com>
2021-02-10 02:40:52 -0800

    more generic types

--- udemy/understanding-typescript/generics/dist/app.js
+++ udemy/understanding-typescript/generics/dist/app.js
@@ -1,13 +1,48 @@
 "use strict";
 function countAndDescribe(element) {
-    let descriptionText = 'Got no value';
-    if (element.length === 0) {
-        descriptionText = 'Got 1 element';
+    let descriptionText = 'Got no value.';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element.';
     }
     else if (element.length > 1) {
         descriptionText = 'Got ' + element.length + ' elements.';
     }
     return [element, descriptionText];
 }
-console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['Sports', 'Cooking']));
+function extractAndConvert(obj, key) {
+    return 'Value: ' + obj[key];
+}
+extractAndConvert({ name: 'Kevin' }, 'name');
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem('Kevin');
+textStorage.addItem('Andrew');
+textStorage.removeItem('Andrew');
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
+function createCourseGoal(title, description, date) {
+    let courseGoal = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal;
+}
+const names = ['Kevin', 'Christian'];
 //# sourceMappingURL=app.js.map
--- udemy/understanding-typescript/generics/src/app.ts
+++ udemy/understanding-typescript/generics/src/app.ts
@@ -48,13 +48,78 @@ interface Lengthy {
 }
 
 function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-   let descriptionText = 'Got no value';
-   if (element.length === 0) {
-      descriptionText = 'Got 1 element';
+   let descriptionText = 'Got no value.';
+   if (element.length === 1) {
+      descriptionText = 'Got 1 element.';
    } else if (element.length > 1) {
       descriptionText = 'Got ' + element.length + ' elements.';
    }
    return [element, descriptionText];
 }
 
-console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['Sports', 'Cooking']));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+   return 'Value: ' + obj[key];
+}
+
+extractAndConvert({ name: 'Kevin' }, 'name');
+
+// simple generic class
+// can store string or numbers but not objects
+// maybe this class will only use primitive values
+class DataStorage<T extends string | number | boolean> {
+   private data: T[] = [];
+
+   addItem(item: T) {
+      this.data.push(item);
+   }
+
+   removeItem(item: T) {
+      if (this.data.indexOf(item) === -1) {
+         return;
+      }
+      this.data.splice(this.data.indexOf(item), 1);
+   }
+
+   getItems() {
+      return [...this.data];
+   }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Kevin');
+textStorage.addItem('Andrew');
+textStorage.removeItem('Andrew');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const kevinObj = {name: 'Kevin'}
+// objStorage.addItem(kevinObj);
+// objStorage.addItem({name: 'Andrew'});
+// // ...
+// objStorage.removeItem(kevinObj);
+// console.log(objStorage.getItems());
+
+// Partial
+interface CourseGoal {
+   title: string;
+   description: string;
+   completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+   let courseGoal: Partial<CourseGoal> = {};
+   courseGoal.title = title;
+   courseGoal.description = description;
+   courseGoal.completeUntil = date;
+   return courseGoal as CourseGoal;
+   // return {title: title, description: description, completeUntil: date};
+}
+
+// Readonly
+const names: Readonly<string[]> = ['Kevin', 'Christian'];
+// names.push('Kevin');
+// names.pop();
