d800d87849bad1ed86bcad4d801e9a6a12faf074
besenio <kbesenio2@yahoo.com>
2021-02-13 01:19:44 -0800

    adding multiple decorators

--- udemy/understanding-typescript/08-decorators/src/app.ts
+++ udemy/understanding-typescript/08-decorators/src/app.ts
@@ -8,6 +8,7 @@
 // decorator factories
 // can now pass in values
 function Logger(logString: string) {
+   console.log('LOGGER FACTORY');
    return function(constructor: Function) {
       console.log(logString);
       console.log(constructor);
@@ -16,7 +17,9 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
    // return function(_: Function) { // the underscore tells TS we don't need the constructor argument
+   console.log('TEMPLATE FACTORY');
    return function(constructor: any) {
+      console.log('Rendering template')
       const hookEl = document.getElementById(hookId);
       const p = new constructor();
       if (hookEl) {
@@ -27,6 +30,8 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 // @Logger('LOGGING - PERSON')
+// WithTemplate runs first and then Logger
+@Logger('LOGGING')
 @WithTemplate('<h1>My Person Object<h1>', 'app')
 class Person {
    name = 'Kevin';
