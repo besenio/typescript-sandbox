461d967277e3fc5e090cf59a9ca02f5d1817d19f
besenio <kbesenio2@yahoo.com>
2021-02-14 22:50:34 -0800

    returning a class in a class decorator

--- udemy/understanding-typescript/08-decorators/src/app.ts
+++ udemy/understanding-typescript/08-decorators/src/app.ts
@@ -18,13 +18,18 @@ function Logger(logString: string) {
 function WithTemplate(template: string, hookId: string) {
    // return function(_: Function) { // the underscore tells TS we don't need the constructor argument
    console.log('TEMPLATE FACTORY');
-   return function(constructor: any) {
-      console.log('Rendering template')
-      const hookEl = document.getElementById(hookId);
-      const p = new constructor();
-      if (hookEl) {
-         hookEl.innerHTML = template;
-         hookEl.querySelector('h1')!.textContent = p.name;
+   return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) {
+      // runs when defined and not instantiated
+      return class extends originalConstructor {
+         constructor(..._: any[]) {
+            super();
+            console.log('Rendering template');
+            const hookEl = document.getElementById(hookId);
+            if (hookEl) {
+               hookEl.innerHTML = template;
+               hookEl.querySelector('h1')!.textContent = this.name;
+            }
+         }
       }
    }
 }
@@ -101,3 +106,8 @@ class Product {
       return this._price * (1 + tax);
    }
 }
+
+// decorator code executes when defined not at runtime when you call a method
+// decorators allows set up work when a class is defined, they are not event listeners
+const p1 = new Product('Book 1', 19);
+const p2 = new Product('Book 2', 29);
