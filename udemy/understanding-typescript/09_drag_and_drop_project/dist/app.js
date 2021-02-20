9d8cdd006c7ce30d7b62e871e4b276d13f7c15f9
besenio <kbesenio2@yahoo.com>
2021-02-19 19:29:08 -0800

    adding a droppable area

--- udemy/understanding-typescript/09_drag_and_drop_project/dist/app.js
+++ udemy/understanding-typescript/09_drag_and_drop_project/dist/app.js
@@ -111,7 +111,8 @@ class ProjectItem extends Component {
         }
     }
     dragStartHandler(event) {
-        console.log(event);
+        event.dataTransfer.setData('text/plain', this.project.id);
+        event.dataTransfer.effectAllowed = 'move';
     }
     dragEndHandler(_) {
         console.log('DragEnd');
@@ -137,7 +138,24 @@ class ProjectList extends Component {
         this.configure();
         this.renderContent();
     }
+    dragOverHandler(event) {
+        if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul');
+            listEl.classList.add('droppable');
+        }
+    }
+    dropHandler(event) {
+        console.log(event.dataTransfer.getData('text/plain'));
+    }
+    dragLeaveHandler(_) {
+        const listEl = this.element.querySelector('ul');
+        listEl.classList.remove('droppable');
+    }
     configure() {
+        this.element.addEventListener('dragover', this.dragOverHandler);
+        this.element.addEventListener('dragleave', this.dragLeaveHandler);
+        this.element.addEventListener('drop', this.dropHandler);
         projectState.addListener((projects) => {
             const relevantProjects = projects.filter(prj => {
                 if (this.type === 'active') {
@@ -162,6 +180,12 @@ class ProjectList extends Component {
         }
     }
 }
+__decorate([
+    autobind
+], ProjectList.prototype, "dragOverHandler", null);
+__decorate([
+    autobind
+], ProjectList.prototype, "dragLeaveHandler", null);
 class ProjectInput extends Component {
     constructor() {
         super('project-input', 'app', true, 'user-input');
--- udemy/understanding-typescript/09_drag_and_drop_project/src/app.ts
+++ udemy/understanding-typescript/09_drag_and_drop_project/src/app.ts
@@ -171,7 +171,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
 
    @autobind
    dragStartHandler(event: DragEvent) {
-      console.log(event);
+      event.dataTransfer!.setData('text/plain', this.project.id);
+      event.dataTransfer!.effectAllowed = 'move';
    }
 
    dragEndHandler(_: DragEvent) {
@@ -191,7 +192,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
 }
 
 // ProjectList Class
-class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
    assignedProjects: Project[];
 
    constructor(private type: 'active' | 'finished') {
@@ -202,7 +203,30 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
       this.renderContent();
    }
 
+   @autobind
+   dragOverHandler(event: DragEvent) {
+      if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+         event.preventDefault();
+         const listEl = this.element.querySelector('ul')!;
+         listEl.classList.add('droppable');
+      }
+   }
+
+   dropHandler(event: DragEvent) {
+      console.log(event.dataTransfer!.getData('text/plain'));
+   }
+
+   @autobind
+   dragLeaveHandler(_: DragEvent) {
+      const listEl = this.element.querySelector('ul')!;
+      listEl.classList.remove('droppable');
+   }
+
    configure() {
+      this.element.addEventListener('dragover', this.dragOverHandler);
+      this.element.addEventListener('dragleave', this.dragLeaveHandler);
+      this.element.addEventListener('drop', this.dropHandler);
+
       projectState.addListener((projects: Project[]) => {
          const relevantProjects = projects.filter(prj => {
             if (this.type === 'active') {
