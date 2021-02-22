2a852bfc2fad4f906104c2ce2bc00307a215a68b
besenio <kbesenio2@yahoo.com>
2021-02-21 22:45:39 -0800

    select and share with google maps

--- udemy/understanding-typescript/13_select_and_share_project/dist/app.js
+++ udemy/understanding-typescript/13_select_and_share_project/dist/app.js
@@ -1,2 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+const form = document.querySelector('form');
+const addressInput = document.getElementById('address');
+const GOOGLE_API_KEY = 'AIzaSyBWXSjgvQ1XwxXuB8MdQpZKYgzc41dKtia';
+function searchAddressHandler(event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    axios_1.default
+        .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+        .then(response => {
+        console.log(response);
+    })
+        .catch(err => {
+        console.log(err);
+    });
+}
+form.addEventListener('submit', searchAddressHandler);
 //# sourceMappingURL=app.js.map
